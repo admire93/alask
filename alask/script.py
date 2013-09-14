010ca4aa94ddcc0382ce82a1093960edd802d28c
Kang Hyojun <admire9@gmail.com>
2013-09-15 03:00:24 +0900

    Add new to generate struct of configuration file

--- alask/script.py
+++ alask/script.py
@@ -13,12 +13,39 @@ from alask.web.app import app
 
 __all__ = 'manager', 'run'
 
+
+configure_form = """
+    DEBUG = True
+
+    DATABASE_URL = '$DATABASE_URL'
+
+    ALEMBIC_SCRIPT_LOCATION = '$ALEMBIC_SCRIPT_LOCATION'
+"""
+
 @Manager
 def manager(config=None):
     config = os.path.abspath(config)
     app.config.from_pyfile(config)
     assert 'DATABASE_URL' in app.config, 'DATABASE_URL missing in config.'
     return app
+
+
+@manager.command
+def new():
+    database_url = input('URL of database '
+                         '(eg. postgresql://user@localhost/dbname)')
+    alembic_script_location = input('Alembic script location MUST CONFIGURED'
+                                    '(eg. alask:migrations)')
+    if not alembic_script_location:
+        raise ValueError('ALEMBIC_SCRIPT_LOCATION MUST be added.')
+
+    configure_form = configure_form.replace('$DATABASE_URL', database_url)
+    configure_form = configure_form.replace('$ALEMBIC_SCRIPT_LOCATION',
+                                            database_url)
+    with open('./gen.cfg.py', 'w') as f:
+        f.write(configure_form)
+
+    print 'Now you can use alask with generated configuration files(gen.cfg.py)'
 
 
 @manager.option('--message', '-m', dest='message', default=None)
@@ -56,7 +83,9 @@ def history(_range=10):
     config, _ = get_alembic_config(engine)
     return alembic_history(config, _range)
 
-@manager.option(manager.add_option('-c', '--config', dest='config', required=True)
+@manager.option(manager.add_option('-c',
+                                   '--config',
+                                   dest='config'))
 
 
 def run():
