01f80f0e81ed6d4cfadb70ad486bbf158c0ceb51
hyojun <hyojun@local.ubuntu>
2013-09-15 02:40:57 +0900

    Add history

--- alask/script.py
+++ alask/script.py
@@ -6,6 +6,7 @@ from flask.ext.script import Manager, prompt_bool
 from alembic.command import revision as alembic_revision
 from alembic.command import upgrade as alembic_upgrade
 from alembic.command import downgrade as alembic_downgrade
+from alembic.command import history as alembic_history
 
 from alask.db import get_alembic_config, get_engine, Base
 from alask.web.app import app
@@ -44,7 +45,18 @@ def downgrade(revision):
     alembic_downgrade(config, revision)
 
 
-manager.add_option('-c', '--config', dest='config', required=True)
+@manger.option('--range', '-n', dest=_range)
+def history(_range=10):
+    try:
+        _range = int(_range)
+    except ValueError:
+        print "range of history MUST be `int` not %s" % str(type(_range))
+
+    engine = get_engine()
+    config, _ = get_alembic_config(engine)
+    return alembic_history(config, _range)
+
+@manager.option(manager.add_option('-c', '--config', dest='config', required=True)
 
 
 def run():
