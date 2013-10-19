92a8ace1d794a627ddeb58afd608c6f6e11439ca
Kang Hyojun <admire9@gmail.com>
2013-10-20 02:44:39 +0900

    Create alaskic without migration directory

--- alask/script.py
+++ alask/script.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+
 from argparse import ArgumentParser
 from subprocess import call
 from string import Template
 
+PROJECT_DIR = ''
 config = {
     'PROJECT_NAME': '',
     'DATABASE_URL': '',
-    'ALEMBIC_SCRIPT_LOCATION': '',
+    'ALEMBIC_SCRIPT_LOCATION': ''
 }
 
 parser = ArgumentParser(description="Start new project with alask.")
-
 parser.add_argument('--name', help='name of new project')
 
 args = parser.parse_args()
@@ -25,11 +27,11 @@ def generate_configuration(template_base_dir, project_base_dir, config):
             wf.write(t.substitute(**config))
 
 
-def generate_tmpl_files(template_base_dir, project_dir, config):
+def generate_tmpl_files(alask_dir, project_dir, config):
     migration = 'migrations', 'env.py.tmpl'
     manager = 'manager.py.tmpl'
-    tmpl_path = os.path.join(project_dir, *migration)
-    tmpl_manager = os.path.join(template_base_dir, manager)
+    tmpl_path = os.path.join(alask_dir, *migration)
+    tmpl_manager = os.path.join(alask_dir, manager)
     with open(tmpl_path) as f:
         t = Template(f.read())
         with open(os.path.join(project_dir,
@@ -44,15 +46,29 @@ def generate_tmpl_files(template_base_dir, project_dir, config):
                   'w') as wf:
             wf.write(t.substitute(**config))
 
-    os.remove(tmpl_path)
-    os.remove(tmpl_manager)
 
+def create_structure(project_dir, alask_dir):
+    project_web = os.path.join(project_dir, 'web')
+    call(['mkdir', '-p', project_dir])
+    call(['mkdir', '-p', config['ALEMBIC_SCRIPT_LOCATION']])
+    call(['mkdir', '-p', os.path.join(config['ALEMBIC_SCRIPT_LOCATION'],
+                                      'versions')])
+    call(['mkdir', '-p', project_web])
+    call(['touch', os.path.join(project_dir, '__init__.py')])
+    call(['touch', os.path.join(project_web, '__init__.py')])
+    call(['cp', os.path.join(alask_dir, 'db.py'), project_dir])
+    call(['cp', os.path.join(alask_dir, 'web', 'app.py'), project_web])
+    call(['cp',
+          os.path.join(alask_dir, 'migrations', 'script.py.mako'),
+          config['ALEMBIC_SCRIPT_LOCATION']])
 def run():
     config['PROJECT_NAME'] = args.name
-    alask_dir = os.path.dirname(os.path.abspath(__file__))
     project_dir = os.path.join(os.getcwd(), config['PROJECT_NAME'])
+    config['ALEMBIC_SCRIPT_LOCATION'] = os.path.join(project_dir, 'migrations')
+    alask_dir = os.path.dirname(os.path.abspath(__file__))
+    create_structure(project_dir, alask_dir)
     alask_base_dir = os.path.join(alask_dir, '..')
     project_base_dir = os.path.join(project_dir, '..')
-    call(['cp', '-r', alask_dir, project_dir])
     generate_configuration(alask_base_dir, project_base_dir, config)
-    generate_tmpl_files(alask_base_dir, project_dir, config)
+    generate_tmpl_files(alask_dir, project_dir, config)
+    call(['echo', 'alaskic created!!'])
