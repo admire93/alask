fac0293584c311404fa4d223b03c15858161a5df
Kang Hyojun <hyojun@admire.kr>
2014-01-20 10:40:36 +0900

    Change version of sqlalchemy

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='alask',
       author='Kang Hyojun',
       author_email='hyojun@admire.kr',
       install_requires=[
-          'flask==0.10.1', 'flask-script==0.5.3', 'sqlalchemy==0.8.2',
+          'flask==0.10.1', 'flask-script==0.5.3', 'sqlalchemy>=0.8.2',
           'alembic==0.6.0', 'pytest==2.3.5',
       ],
       packages=find_packages(),
